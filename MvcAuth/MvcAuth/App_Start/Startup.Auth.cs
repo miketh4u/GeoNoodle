15c9718bd2e806749792fb3e2b14a17fb60a8889
jmt4 <jimmtriphahn@gmail.com>
2015-09-01 23:10:16 -0700

    implemented social sign in using google+

--- MvcAuth/MvcAuth/App_Start/Startup.Auth.cs
+++ MvcAuth/MvcAuth/App_Start/Startup.Auth.cs
@@ -58,11 +58,11 @@ namespace MvcAuth
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "******",
+                ClientSecret = "*****"
+            });
         }
     }
 }
